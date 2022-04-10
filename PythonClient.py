f88c2feade8492d62448c1adc686b62336540bcf
syx2259 <syx2259@gmail.com>
2022-04-10 14:42:31 -0400

    Update PythonClient.py

--- PythonClient.py
+++ PythonClient.py
@@ -35,11 +35,11 @@ def requestOne(host, port, index):
     client = Example.Client(protocol)
     transport.open()
     if index == 0:
-        #f = open("rtt.txt", "w")
+        f = open("rtt.txt", "w")
         #f = open("rtt_one_cpu_intensive.txt", "w")
         #f = open("rtt_two_cpu_intensive.txt", "w")
         #f = open("rtt_three_cpu_intensive.txt", "w")
-        f = open("rtt_four_cpu_intensive.txt", "w")
+        #f = open("rtt_four_cpu_intensive.txt", "w")
         for x in range(100000):
             startTime = time.time()
             # Run showCurrentTimestamp() method on server
@@ -76,40 +76,10 @@ if __name__ == "__main__":
     try:
         #hosts = ("10.0.2.21", "10.0.2.22", "10.0.2.23", "10.0.2.24", "10.0.2.25")
         #hosts = ("10.0.2.21", "10.0.2.22")
+        requestOne("10.0.2.21", 9090, 1)
         #results = requestAll(hosts)
         #for result in results:
         #    print(result)
-        requestOne("10.0.2.21", 9090, 0)
-        # Init thrift connection and protocol handlers
-        # transport = TSocket.TSocket( host , port)
-        # transport = TTransport.TBufferedTransport(transport)
-        # protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-        # # Set client to our Example
-        # client = Example.Client(protocol)
-
-        # # Connect to server
-        # transport.open()
-
-        # # startTime of RTT
-        # startTime = time.time()
-
-        # # Run showCurrentTimestamp() method on server
-        # currentTime = client.showCurrentTimestamp()
-
-        # # Calculate the RTT
-        # endTime = time.time()
-        # print("RTT: " + str(endTime - startTime))
-        # print("Request result: " + currentTime)
-
-        # Assume that you have a job which takes some time
-        # but client sholdn't have to wait for job to finish
-        # ie. Creating 10 thumbnails and putting these files to sepeate folders
-        #client.asynchronousJob()
-
-
-        # Close connection
-        # transport.close()
     except Thrift.TException as tx:
         print('Something went wrong : %s' % (tx.message))
 
